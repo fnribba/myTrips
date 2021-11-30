0168cf19a5a8bec02445d67208aa8d79f1e3649a
fnribba <facu.ribba1@gmail.com>
2021-11-30 13:01:04 -0300

    Creé variables de SASS, utilicé extends, agrege nuevos archivos y estilos

--- scripts.js
+++ scripts.js
@@ -6,3 +6,10 @@ menuToggle.onclick = function(){
         menuToggle.classList.toggle('active')
         navbar__ul.classList.toggle('active')
     }
+
+function scrollHeader(){
+        const header = document.getElementById('header')
+        // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+        if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    }
+    window.addEventListener('scroll', scrollHeader)
